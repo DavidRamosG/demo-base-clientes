95837ff0a0004ad629737c7536ff86b93105cbf1
DavidRamosG <david.ramosg@gmail.com>
2024-02-05 21:15:44 +0100

    Nodos necesarios fauna y node-fetch

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,12 @@
 exports.handler = async () => {
-    return {
+    /*return {
       statusCode: 200,
       body: "Hola Mundo Cruel!"
+    };*/
+
+    const mySecret = process.env.STRIPE_SECRET_KEY;
+    return {
+      statusCode: 200,
+      body: `La clave de stripe es: ${mySecret}`
     };
   };
